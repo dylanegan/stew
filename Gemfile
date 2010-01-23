61ee36a245428843174f4b4cab84463ff447a2e4
Dylan Egan <me@dylanegan.com>
2010-01-23 17:21:51 +1100

    Bundle bunny for manual client example.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 gem "amqp", "0.6.5"
+gem "bunny", "0.6.0"
 gem "exemplor", "2010.0.0"
 
 disable_system_gems
--- examples/manual/client.rb
+++ examples/manual/client.rb
@@ -1,9 +1,8 @@
-require 'rubygems'
+require File.dirname(__FILE__) + '/../../vendor/gems/environment'
 require 'bunny'
 
 bunny = Bunny.new
 bunny.start
 
 direct = bunny.exchange('example')
-
 direct.publish('example payload')
